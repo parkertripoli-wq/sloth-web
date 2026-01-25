aeccbc37b347dc1c504323c48529a1e6697f735d
PJT <230787001+parkertripoli-wq@users.noreply.github.com>
2026-01-24 20:46:14 -0800

    Add download links for different platforms in installer.bat

--- installer.bat
+++ installer.bat
@@ -1,3 +1,5 @@
+Download Website at https://parkertrip.github.io/slothweb/download/
+
 Mac/Apple computer:    https://github.com/parkertripoli-wq/sloth-web/blob/main/installer_mac.sh
 Windows/Microsoft:    https://github.com/parkertripoli-wq/sloth-web/blob/main/installer_windows.bat
 Linux/Linux distrobuton:    https://github.com/parkertripoli-wq/sloth-web/blob/main/installer_linux.sh
